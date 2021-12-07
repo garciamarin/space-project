ee2862d40cac067616ec15910a35683d37fe1bb7
valeriofichera <valeriofichera@yahoo.com>
2021-12-07 16:02:46 +0100

    btc_ticker addded

--- src/Tab.js
+++ src/Tab.js
@@ -6,14 +6,14 @@ import Typography from '@mui/material/Typography';
 
 import NewsSearch from './NewsSearch';
 import Live3 from './Live3';
-
+import GetBtc from './btc_ticker';
 
 export default function Tab() {
     const params = useParams();
 
     const TabContent = {
         '1':<Typography mx="auto"  textAlign='center' variant='h2' > 
-                Live {params.value}
+                <GetBtc/> 
             </Typography>,
 
         '2':<NewsSearch/>,
---
+++ src/btc_ticker.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ConstructionRounded } from '@mui/icons-material';
+import { Box, Typography, Paper } from '@mui/material';
+import {useState} from "react";
+
+
+export default function GetBtc() {
+    
+    const [btc, getBtc] = useState("");
+   
+       fetch(`https://api.nomics.com/v1/currencies/ticker?key=197d432dcb8da0bea2f38c965b1697a78f2dea9e&ids=BTC&interval=1h&convert=USD&per-page=1&page=1`)
+       .then((res) => res.json())
+       .then((data) => {
+            getBtc(data[0].id + " Price: " + data[0].price + "  USD");
+            });
+    return(
+        <div>
+            {btc}
+        </div>
+
+    )
+};
